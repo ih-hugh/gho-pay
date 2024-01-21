bedfc6ce38b93ff1c7902b7f088c1c254e67690a
JoelCCodes <32679235+JoelCCodes@users.noreply.github.com>
2024-01-20 23:06:35 -0500

    ...

---
+++ packages/nextjs/hooks/scaffold-eth/useAAVEData.ts
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import { ChainId, UiPoolDataProvider } from "@aave/contract-helpers";
+import { formatUserSummary } from "@aave/math-utils";
+import { formatReserves } from "@aave/math-utils";
+import * as markets from "@bgd-labs/aave-address-book";
+import dayjs from "dayjs";
+import { ethers } from "ethers";
+
+const useAaveData = (address: any, window: any) => {
+  const [data, setData] = useState({
+    formattedPoolReserves: {},
+    userSummary: {},
+  });
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState({});
+
+  useEffect(() => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum as any);
+
+    const poolDataProviderContract = new UiPoolDataProvider({
+      uiPoolDataProviderAddress: markets.AaveV3Sepolia.UI_POOL_DATA_PROVIDER,
+      provider,
+      chainId: ChainId.mainnet,
+    });
+
+    const getAAVEData = async () => {
+      try {
+        const reserves = await poolDataProviderContract.getReservesHumanized({
+          lendingPoolAddressProvider: markets.AaveV3Sepolia.POOL_ADDRESSES_PROVIDER,
+        });
+        const userReserves = await poolDataProviderContract.getUserReservesHumanized({
+          lendingPoolAddressProvider: markets.AaveV3Sepolia.POOL_ADDRESSES_PROVIDER,
+          user: address,
+        });
+
+        const reservesArray = reserves.reservesData;
+        const baseCurrencyData = reserves.baseCurrencyData;
+        const userReservesArray = userReserves.userReserves;
+        const currentTimestamp = dayjs().unix();
+
+        const formattedPoolReserves = formatReserves({
+          reserves: reservesArray,
+          currentTimestamp,
+          marketReferenceCurrencyDecimals: baseCurrencyData.marketReferenceCurrencyDecimals,
+          marketReferencePriceInUsd: baseCurrencyData.marketReferenceCurrencyPriceInUsd,
+        });
+
+        const userSummary = formatUserSummary({
+          currentTimestamp,
+          marketReferencePriceInUsd: baseCurrencyData.marketReferenceCurrencyPriceInUsd,
+          marketReferenceCurrencyDecimals: baseCurrencyData.marketReferenceCurrencyDecimals,
+          userReserves: userReservesArray,
+          formattedReserves: formattedPoolReserves,
+          userEmodeCategoryId: userReserves.userEmodeCategoryId,
+        });
+
+        setData({ formattedPoolReserves, userSummary });
+      } catch (error) {
+        setError(error as any);
+      } finally {
+        setLoading(false);
+      }
+    };
+    if (address && window) {
+      getAAVEData();
+    }
+  }, [address, window]);
+
+  return { data, loading, error };
+};
+
+export default useAaveData;
